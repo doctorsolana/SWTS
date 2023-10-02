a94fda07953a48734ed00ebb5018de8e28befa6f
Your Name <you@example.com>
2023-10-02 18:46:41 +0200

    first

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -9,20 +9,20 @@ export function Home() {
     <div className={styles.banner}>
       <Section>
         <h2>
-          What is this?
+          SWEET GAMES
         </h2>
         <p style={{ wordWrap: 'break-word' }}>
-          An open source, decentralized casino built on Gamba. Copy this platform with your own branding and start earning fees on every bet.
+          An open source, decentralized casino built on Solana.
         </p>
         <div style={{ display: 'flex', gap: '10px' }}>
           <Button
             as="a"
             color="white"
-            href="https://gamba.so"
+            href="https://sweetsmart.xyz/pages/how-to-buy-our-token"
             target="_blank"
             icon={<Icon.ExternalLink />}
           >
-            Learn more
+            Buy $SWTS
           </Button>
         </div>
       </Section>
